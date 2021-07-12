96786d73df16064e8b1a5827bfc0e5c8d33d8926
guillaume-boutin <guillaumeboutin2101@gmail.com>
2021-07-12 16:37:11 -0400

    fixed issue with certificate volume.

--- certificate/create.sh
+++ certificate/create.sh
@@ -14,3 +14,5 @@ DOMAIN=$1
 openssl pkcs12 -inkey /home/cert/ssl/$DOMAIN.pem -in /home/cert/ssl/$DOMAIN.crt -export -out /home/cert/ssl/$DOMAIN.p12 -password pass:secret
 
 echo ""
+
+chown -R cert:cert /home/cert/ssl
